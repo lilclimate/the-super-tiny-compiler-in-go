4ac5a2d4eafd1e90c65008225480f3a99bec5155
lil climate <lilclimate@gmail.com>
2022-12-01 16:58:15 +0800

    feat: 词法解析拆分任务

---
+++ tokenizer_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+// combination  expression: (add 2 (subtract 4 2))
+// single expression: (add 1 2)
+// single number: 2
+// single paren: (  / )
+
+// func Test
+func TestCombinationExpression(t *testing.T) {
+	t.Skip(".")
+}
+
+func TestSingleExpression(t *testing.T) {
+	t.Skip(".")
+}
+
+func TestParen(t *testing.T) {
+	t.Skip(".")
+}
+
+func TestNumber(t *testing.T) {
+	t.Skip(".")
+}
