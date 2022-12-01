b1b3fa5cb7a48a6aa95c7be13def8dd0c7f60867
lil climate <lilclimate@gmail.com>
2022-12-01 17:25:21 +0800

    feat: 词法分析实现左括号

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // combination  expression: (add 2 (subtract 4 2))
 // single expression: (add 1 2)
@@ -17,9 +21,40 @@ func TestSingleExpression(t *testing.T) {
 }
 
 func TestParen(t *testing.T) {
-	t.Skip(".")
+	tokens := []token{
+		{
+			kind:  "Paren",
+			value: "(",
+		},
+	}
+
+	code := "("
+	assert.Equal(t, tokens, tokenizer(code))
+}
+
+func tokenizer(code string) []token {
+	tokens := []token{}
+	current := 0
+	for current < len(code) {
+		char := string(code[current])
+
+		if char == "(" {
+			tokens = append(tokens, token{
+				kind:  "Paren",
+				value: "(",
+			})
+			current++
+		}
+
+	}
+	return tokens
 }
 
 func TestNumber(t *testing.T) {
 	t.Skip(".")
+}
+
+type token struct {
+	kind  string
+	value string
 }
